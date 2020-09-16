14e297e1e4f134dc2abc990011c1132964d9f0d9
Arvinder <arvinder_kamboo@hotmail.com>
2020-09-16 05:00:05 +0100

    Section 4 update

---
+++ StarChart/Controllers/CelestialObjectsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StarChart.Data;
+
+namespace StarChart.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class CelestialObjectsController : ControllerBase
+    {
+        public ApplicationDbContext _context { get; set; }
+
+        public CelestialObjectsController(ApplicationDbContext context) => _context = context;
+    }
+}
